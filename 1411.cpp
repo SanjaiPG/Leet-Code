0e2fe7a60921744ccc3971f0772c63e485a48b83
SanjaiPG <sanjaigukesan8686@gmail.com>
2026-01-03 22:28:41 +0530

    daily qn

---
+++ 1411.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int numOfWays(int n)
+    {
+
+        const int MOD = 1e9 + 7;
+
+        long long A = 6;
+        long long B = 6;
+
+        for (int i = 2; i <= n; i++)
+        {
+            long long newA = (A * 3 + B * 2) % MOD;
+            long long newB = (A * 2 + B * 2) % MOD;
+            A = newA;
+            B = newB;
+        }
+
+        return (A + B) % MOD;
+    }
+};
